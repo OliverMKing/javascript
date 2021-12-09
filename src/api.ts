a51c8cca874d6c08abf47267d4a5ddf25075abec
Oliver King <kingoliver@microsoft.com>
2021-12-09 09:44:29 -0500

    add node-fetch

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,15 @@
+import fetch, { Request, Headers, Response } from 'node-fetch';
+
+// inject node-fetch
+if (!globalThis.fetch) {
+    // @ts-ignore
+    globalThis.fetch = fetch;
+    // @ts-ignore
+    globalThis.Headers = Headers;
+    // @ts-ignore
+    globalThis.Request = Request;
+    // @ts-ignore
+    globalThis.Response = Response;
+}
+
 export * from './gen/api';
